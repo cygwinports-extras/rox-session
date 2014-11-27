4637d341ba6d59613eba9fd6bb433960408f4442
Yaakov Selkowitz <yselkowi@redhat.com>
2014-11-27 14:08:21 -0600

    Rebuilt for xinit changes

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 /usr/bin/mkdir -p "$(/usr/bin/cygpath $CYGWINFORALL -P)/Cygwin-X"
-/usr/bin/mkshortcut $CYGWINFORALL -P -i /usr/bin/XWin.exe -n "Cygwin-X/ROX" -a "/usr/bin/bash.exe -l -c \"/usr/bin/startx /usr/bin/dbus-launch rox-session -w\"" /usr/bin/run.exe
+/usr/bin/mkshortcut $CYGWINFORALL -P -w / -i /usr/bin/XWin.exe -n "Cygwin-X/ROX" -a "--quote /usr/bin/bash.exe -l -c \"cd; /usr/bin/startx /etc/X11/xinit/Xsession rox-session -w\"" /usr/bin/run.exe
