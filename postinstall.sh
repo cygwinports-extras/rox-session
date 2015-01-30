9652b90eed822957a3d139f8e79fb433235efaa3
Yaakov Selkowitz <yselkowi@redhat.com>
2015-01-30 15:19:39 -0600

    Rebuilt for latest xinit changes

--- postinstall.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/mkdir -p "$(/usr/bin/cygpath $CYGWINFORALL -P)/Cygwin-X"
-/usr/bin/mkshortcut $CYGWINFORALL -P -w / -i /usr/bin/XWin.exe -n "Cygwin-X/ROX" -a "--quote /usr/bin/bash.exe -l -c \"cd; /usr/bin/startx /etc/X11/xinit/Xsession rox-session -w\"" /usr/bin/run.exe
--- preremove.sh
+++
@@ -1,2 +0,0 @@
-rm -f "$(cygpath $CYGWINFORALL -P)/Cygwin-X/ROX.lnk"
-rmdir --ignore-fail-on-non-empty "$(cygpath $CYGWINFORALL -P)/Cygwin-X"
